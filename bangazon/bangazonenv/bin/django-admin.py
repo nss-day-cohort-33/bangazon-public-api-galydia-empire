16b6fdc5e2f08a21f0f769dbd339fa7b58e6c87c
scottsilver <scottsilver47@gmail.com>
2019-10-02 15:40:16 -0500

    initial commit

---
+++ bangazon/bangazonenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/scottsilver/workspace/python-stuff/bangazon-public-api-galydia-empire/bangazon/bangazonenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
