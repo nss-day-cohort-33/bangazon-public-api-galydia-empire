5592ab46727c2609e1b26141aefced3cb6442e88
Jeff Hill <jeffhill1978@gmail.com>
2019-10-15 10:59:53 -0500

    updated models, nuked, remigrated

--- bangazon/bangazonapi/models/orderproduct.py
+++ bangazon/bangazonapi/models/orderproduct.py
@@ -11,8 +11,8 @@ Method: None
 """
 class OrderProduct(models.Model):
 
-    order = models.ForeignKey(Order, on_delete=models.DO_NOTHING)
-    product= models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product= models.ForeignKey(Product, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ("order", )
